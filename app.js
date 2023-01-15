92f3a04a2f73817a1d624c5f01722d181be4320f
Raviteja Karavadi <krtproperty72@gmail.com>
2023-01-15 10:10:58 +0530

    Uploading NodeJS Files

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// const errorController = require("./controllers/error");
+const products = require('./controllers/products');
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+app.use(products.error);
+
+app.listen(3000);
